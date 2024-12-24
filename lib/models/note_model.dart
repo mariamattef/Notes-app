048309752f240be8d0c8ee6dfd6fa429f8f1afb5
mariamattef <121569095+mariamattef@users.noreply.github.com>
2024-12-24 10:19:04 +0200

    create note model and add hive pachage

---
+++ lib/models/note_model.dart
@@ -0,0 +1,12 @@
+class NoteModel {
+  final String titlr;
+  final String subtitle;
+  final String date;
+  final int color;
+
+  NoteModel(
+      {required this.titlr,
+      required this.subtitle,
+      required this.date,
+      required this.color});
+}
